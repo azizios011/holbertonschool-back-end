006a327738d877e2980f5a65e17bb960a0d1536a
azizios011 <hdriaziz699@gmail.com>
2023-09-10 10:02:12 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,12 +3,17 @@
 import requests
 import sys
 
+
 def check_arg():
     num_arg = len(sys.argv)
-    if num_arg != 2:
-        print("Usage: python3 script.py <employee_id>")
+    if num_arg == 1:
+        print("Error: missing argument")
         sys.exit(1)
-    return int(sys.argv[1])
+    elif num_arg > 2:
+        print("Error: too many arguments")
+        sys.exit(1)
+    else:
+        return int(sys.argv[1])
 
 employee_id = check_arg()
 
@@ -31,16 +36,11 @@ for task in data:
     if task["completed"]:
         completed_tasks += 1
     if not employee_name:
-        employee_name = task.get("name")  
-
-if employee_name is None:
-    print("Error: Employee name not found in the API response")
-    sys.exit(1)
-
-employee_name = "OK"
+        employee_name = task.get("name")
 
 print("Employee {} is done with tasks({}/{}):"
       .format(employee_name, completed_tasks, total_tasks))
+
 
 for task in data:
     if task["completed"]:
