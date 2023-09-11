4fb70990d765ba686c2623558359ae4f332e61a7
azizios011 <hdriaziz699@gmail.com>
2023-09-11 16:23:53 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
+"""Script that uses a REST API to get information about an employee's TODO list progress"""
 
 import requests
 import sys
 
-
-def check_arg():
-    num_arg = len(sys.argv)
-    if num_arg == 1:
-        print("Error: missing argument")
+if __name__ == "__main__":
+    # Get the employee ID from the command line argument
+    employee_id = sys.argv[1]
+    # Validate that the employee ID is an integer
+    if not employee_id.isdigit():
+        print("Usage: {} <employee ID>".format(sys.argv[0]))
         sys.exit(1)
-    elif num_arg > 2:
-        print("Error: too many arguments")
-        sys.exit(1)
-    else:
-        return int(sys.argv[1])
-
-employee_id = check_arg()
-
-url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
-
-response = requests.get(url)
-
-if response.status_code != 200:
-    print("Error: Unable to fetch data from the API")
-    sys.exit(1)
-
-data = response.json()
-
-total_tasks = 0
-completed_tasks = 0
-employee_name = None
-
-for task in data:
-    total_tasks += 1
-    if task["completed"]:
-        completed_tasks += 1
-    if not employee_name:
-        employee_name = task["userId"]
-
-print("Employee {} is done with tasks({}/{}):"
-      .format(employee_name, completed_tasks, total_tasks))
-
-
-for task in data:
-    if task["completed"]:
-        print("\t {}".format(task["title"]))
+    # Make a GET request to the API endpoint for users
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    users_response = requests.get(users_url)
+    # Check if the response status code is 200 (OK)
+    if users_response.status_code == 200:
+        # Get the JSON data from the response
+        users_data = users_response.json()
+        # Get the employee name from the data
+        employee_name = users_data.get("name")
+        # Make a GET request to the API endpoint for todos
+        todos_url = "https://jsonplaceholder.typicode.com/todos"
+        todos_response = requests.get(todos_url, params={"userId": employee_id})
+        # Check if the response status code is 200 (OK)
+        if todos_response.status_code == 200:
+            # Get the JSON data from the response
+            todos_data = todos_response.json()
+            # Initialize variables to store the total number of tasks and the number of done tasks
+            total_tasks = 0
+            done_tasks = 0
+            # Initialize a list to store the titles of completed tasks
+            done_titles = []
+            # Loop through the todos data
+            for todo in todos_data:
+                # Increment the total number of tasks by 1
+                total_tasks += 1
+                # Check if the task is completed
+                if todo.get("completed"):
+                    # Increment the number of done tasks by 1
+                    done_tasks += 1
+                    # Append the task title to the list of done titles
+                    done_titles.append(todo.get("title"))
+            # Print the first line of output with the employee name and the task progress
+            print("Employee {} is done with tasks({}/{})".format(employee_name, done_tasks, total_tasks))
+            # Loop through the list of done titles
+            for title in done_titles:
+                # Print each title with a tabulation and a space before it
+                print("\t {}".format(title))
