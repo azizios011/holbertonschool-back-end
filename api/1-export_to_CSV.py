2640265ed633f0a90d53c25061db3bb5ee07e6e9
azizios011 <hdriaziz699@gmail.com>
2023-09-13 16:46:01 +0100

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This module exports task data to CSV format.
+
+Author: Your Name
+Date: September 13, 2023
+"""
+
 
 import csv
 import requests
@@ -9,9 +16,19 @@ users_url = "https://jsonplaceholder.typicode.com/users?id="
 todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 def user_info(id):
+
+     """
+    Retrieve user information and export tasks to CSV.
+
+    Args:
+        id (int): The user's ID.
+
+    Returns:
+        None
+    """
+
     total_tasks = 0
 
-    # Fetch tasks associated with the user
     response = requests.get(todos_url).json()
     for task in response:
         if task['userId'] == id:
@@ -20,7 +37,7 @@ def user_info(id):
     num_lines = 0
     with open(str(id) + ".csv", 'r') as f:
         for line in f:
-            if not line.strip():  # Ignore empty lines
+            if not line.strip():
                 continue
             num_lines += 1
 
