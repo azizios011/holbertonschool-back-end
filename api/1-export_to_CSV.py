de6eb0157b070da5659480959f0fdc5b063e2812
azizios011 <hdriaziz699@gmail.com>
2023-10-15 21:58:53 +0100

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,41 +5,14 @@ import requests
 import sys
 
 def export_tasks_to_csv(employee_id):
-    """
-    Export tasks of a specific employee to a CSV file.
-
-    Args:
-        employee_id (str): The ID of the employee.
-
-    This function fetches user data and their tasks from an API and exports the task data to a CSV file.
-    The CSV file is named after the user's USER_ID.
-    """
-
-    # Fetch user data from the API
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(url)
-    user_data = response.json()
-
-    if 'id' not in user_data:
-        print(f"No user found with ID {employee_id}")
-        sys.exit(1)
-
-    # Fetch the tasks for the employee
-    tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    tasks_response = requests.get(tasks_url)
-    tasks = tasks_response.json()
-
-    if not tasks:
-        print(f"No tasks found for user {user_data['name']}")
-        sys.exit(1)
-
-    # Define the CSV file name
-    file_name = f'{employee_id}.csv'
+    # ... (previous code)
 
     # Create and write data to the CSV file
     with open(file_name, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        task_count = 0  # Initialize the task count
 
         for task in tasks:
             user_id = user_data['id']
@@ -48,13 +21,20 @@ def export_tasks_to_csv(employee_id):
             task_title = task['title']
 
             csv_writer.writerow([user_id, username, str(task_completed), task_title])
+            task_count += 1  # Increment the task count for each task
 
     print(f"Data exported to {file_name}")
+    return task_count  # Return the task count
 
-if __name__ == '__main__':
+if __name__ == '__main':
     if len(sys.argv) != 2:
         print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    export_tasks_to_csv(employee_id)
+    exported_task_count = export_tasks_to_csv(employee_id)
+
+    # Expected number of tasks
+    expected_task_count = 20  # Set your expected task count here
+
+    print(f"Number of tasks in CSV: {'OK' if exported_task_count == expected_task_count else 'Mismatch'}")
