e71f8abdb4cf5aa102071f80008a48a22f37446e
azizios011 <hdriaziz699@gmail.com>
2023-10-26 19:59:16 +0100

    fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
 """
-Uses the JSON placeholder API to query data about an employee and export it to CSV.
+Uses the JSON placeholder api to query data about an employee
 """
 
+import requests
 import csv
-from requests import get
-from sys import argv
+import sys
 
-if __name__ == '__main':
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        sys.exit(1)
 
-    employee_id = argv[1]
+    employee_id = sys.argv[1]
+
     main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/users/{}/todos".format(employee_id)
+    todo_url = main_url + "/user/{}/todos".format(employee_id)
     name_url = main_url + "/users/{}".format(employee_id)
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    todo_result = requests.get(todo_url).json()
+    name_result = requests.get(name_url).json()
 
     if not todo_result:
-        print("Employee not found or has no tasks.")
-        exit(1)
+        print("No TODO data found for the provided employee ID.")
+        sys.exit(1)
 
-    user_id = name_result[0].get("id")
-    username = name_result[0].get("username")
+    employee_name = name_result.get("name")
 
-    # CSV file name
-    csv_file = "{}.csv".format(user_id)
+    # Create a CSV file with the user ID as the filename
+    csv_filename = f"{employee_id}.csv"
 
-    with open(csv_file, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
         for todo in todo_result:
+            task_completed_status = "True" if todo.get("completed") else "False"
             task_title = todo.get("title")
-            task_completed = todo.get("completed")
-            writer.writerow([user_id, username, task_completed, task_title])
+            csv_writer.writerow([employee_id, employee_name, task_completed_status, task_title])
 
-    print("Number of tasks in CSV: OK")
-    print("User ID and Username: OK")
-    print("Formatting: OK")
+    print(f"Data exported to {csv_filename}.")
