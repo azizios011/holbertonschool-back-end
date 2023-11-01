4d505503ff83069aa1fd388ed4484cf76d47a992
azizios011 <hdriaziz699@gmail.com>
2023-11-01 13:31:27 +0100

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     # Count the tasks in the CSV
     with open(csv_filename, 'r', newline='') as csvfile:
         csv_reader = csv.reader(csvfile)
-        task_count = len(list(csv_reader)) - 1  # Subtract 1 for the header row
+        task_count = len(list(csv_reader))  # Subtract 1 for the header row
 
     print(f"Data exported to {csv_filename}.")
     if task_count == len(todo_result):
